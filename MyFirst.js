e916eb3e0e3b1cab3385fe62ad1ff7914880e474
bkkyy018 <bikashghadai5856@outlook.com>
2024-12-22 20:51:01 +0530

    till now

--- MyFirst.js
+++ MyFirst.js
@@ -1,6 +1,14 @@
+//http--> its a module 
 var http = require('http');
-
+//Now application has access to the http and now it can create the server
+var dt=require("./MyFirstModuleFile") 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World!');
-}).listen(8080); 
+    //1-->its for if server is supposed to be displayed as HTML, 
+    //2-->  200 means that all is OK, the second argument is an object containing the response headers.
+    //3-->req argument that represents the request from the client
+    //4-->
+    res.write("The time now is "+dt.MyDateTime());
+    res.write("The http url request by the user is ::-"+req.url)
+    res.end();
+}).listen(8080)
---
+++ MyFirstModuleFile.js
@@ -0,0 +1,5 @@
+exports.MyDateTime=function()
+//exports keyword to make properties and methods available outside the module file.
+{
+ return Date()
+}
